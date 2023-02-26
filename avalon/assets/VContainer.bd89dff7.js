29d825bec3f993d82202e76c03aebfcddea70c6b
Alvin Lee <alvinlee8065@outlook.com>
2023-02-26 10:25:57 +0800

    v1.6.1

---
+++ avalon/assets/VContainer.bd89dff7.js
@@ -0,0 +1,1 @@
+import{m as o,u as n}from"./VGrid.849a3e21.js";import{l as r,t as s}from"./index.9aefa69a.js";const m=r()({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...o()},setup(e,a){let{slots:t}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},t)),{}}});export{m as V};
---
+++ avalon/assets/VGrid.849a3e21.js
@@ -0,0 +1,1 @@
+import{g as r,p as n}from"./index.9aefa69a.js";function a(e){const t=r("useRender");t.render=e}const o=n({tag:{type:String,default:"div"}},"tag");export{o as m,a as u};
